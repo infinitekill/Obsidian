99310d96edcdd42aa74b26fe78ac080165124e1b
infinitekill <113178440+infinitekill@users.noreply.github.com>
2026-02-01 22:10:40 +0200

    Update LibraryTesting.lua

--- LibraryTesting.lua
+++ LibraryTesting.lua
@@ -4300,33 +4300,60 @@ do
             Slider:Display()
         end
 
-        Bar.InputBegan:Connect(function(Input: InputObject)
+        Bar.InputBegan:Connect(function(Input)
             if not IsClickInput(Input) or Slider.Disabled then
                 return
             end
-
-            for _, Side in Library.ActiveTab.Sides do
+        
+            for _, Side in pairs(Library.ActiveTab.Sides) do
                 Side.ScrollingEnabled = false
             end
-
-            while IsDragInput(Input) do
-                local Location = Mouse.X
-                local Scale = math.clamp((Location - Bar.AbsolutePosition.X) / Bar.AbsoluteSize.X, 0, 1)
-
+        
+            local function updatePosition(posX)
+                local Scale = math.clamp((posX - Bar.AbsolutePosition.X) / Bar.AbsoluteSize.X, 0, 1)
+        
                 local OldValue = Slider.Value
-                Slider.Value = Round(Slider.Min + ((Slider.Max - Slider.Min) * Scale), Slider.Rounding)
-
+                Slider.Value = Round(Slider.Min + ((Slider.Max - Slider.Min) * Scale), Info.Rounding)
+        
                 Slider:Display()
                 if Slider.Value ~= OldValue then
                     Library:SafeCallback(Slider.Callback, Slider.Value)
                     Library:SafeCallback(Slider.Changed, Slider.Value)
                 end
-
-                RunService.RenderStepped:Wait()
             end
-
-            for _, Side in Library.ActiveTab.Sides do
-                Side.ScrollingEnabled = true
+        
+            if Input.UserInputType == Enum.UserInputType.Touch then
+        
+                updatePosition(Input.Position.X)
+                local moveConnection, endConnection
+        
+                moveConnection = UserInputService.InputChanged:Connect(function(changedInput)
+                    if changedInput == Input then
+                        updatePosition(changedInput.Position.X)
+                    end
+                end)
+            
+                endConnection = UserInputService.InputEnded:Connect(function(endedInput)
+                    if endedInput == Input then
+                        moveConnection:Disconnect()
+                        endConnection:Disconnect()
+            
+                        for _, Side in pairs(Library.ActiveTab.Sides) do
+                            Side.ScrollingEnabled = true
+                        end
+                    end
+                end)
+            else
+                while IsClickInput(Input) do
+                    local Location = Mouse.X
+                    updatePosition(Location)
+        
+                    RunService.RenderStepped:Wait()
+                end
+        
+                for _, Side in pairs(Library.ActiveTab.Sides) do
+                    Side.ScrollingEnabled = true
+                end
             end
         end)
 
